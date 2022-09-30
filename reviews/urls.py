330af3ea1152add61f97e508377399dd13be21bb
jupiter6676 <jupiter6676@naver.com>
2022-09-30 13:13:52 +0900

    220930 Read (Detail)

--- reviews/urls.py
+++ reviews/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('new/', views.new, name='new'),
     path('create/', views.create, name='create'),
+    path('detail/<int:pk>', views.detail, name='detail'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render, redirect
 from .models import Review
 
 # Create your views here.
+
+# 글 목록(게시판)을 보여줌.
 def index(request):
-    return render(request, 'reviews/index.html')
+    posts = Review.objects.all()
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'reviews/index.html', context)
 
 # 글 작성 페이지 보여줌.
 def new(request):
@@ -18,3 +26,14 @@ def create(request):
 
     # 글 세부 페이지로 이동 (지금은 인덱스로)
     return redirect('reviews:index')
+
+# 리뷰 보기 페이지
+def detail(request, pk):
+    post = Review.objects.get(id=pk)
+
+    context = {
+        'title': post.title,
+        'content': post.content,
+    }
+
+    return render(request, 'reviews/detail.html', context)
